60e7a4017eecdbf888319fd195cc8473b8cd268c
Murtaza Amin Najar <springnexaa@gmail.com>
2026-09-15 21:33:08 +0530

    Remove Nexa Pharma from Nexa-AI

--- apps/nexa-erp-pharma/desktop/preload.cjs
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-contextBridge.exposeInMainWorld('nexaERP', {
-  getData: () => ipcRenderer.invoke('nexa:get-data'),
-  saveData: data => ipcRenderer.invoke('nexa:save-data', data),
-  queueSync: event => ipcRenderer.invoke('nexa:queue-sync', event),
-});
